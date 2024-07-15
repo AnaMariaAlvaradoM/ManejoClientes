e871039e4dbe7846c00e0b74e213be1f495a4c58
Ana Maria Alvarado M <110789128+AnaMariaAlvaradoM@users.noreply.github.com>
2024-07-15 07:49:25 -0500

    Si

--- src/main/java/com/example/manejo_clientes/WebConfig.java
+++ src/main/java/com/example/manejo_clientes/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://669515f0c891101b91a26ddb--frontclientes.netlify.app/")
+                .allowedOrigins("https://frontclientes.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
